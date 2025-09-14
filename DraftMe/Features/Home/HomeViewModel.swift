5d70a77a7c1585e2c6c3a222736a474b290783e3
Kamal K Bhardwaj <kkbhardwaj20@gmail.com>
2025-09-14 21:09:01 +0530

    Add folders

---
+++ DraftMe/Features/Home/HomeViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewModel.swift
+//  DraftMe
+//
+//  Created by Kamal Kishor on 14/09/25.
+//
+
+import Foundation
+
+@Observable
+final class HomeViewModel {
+
+    init() {
+        
+    }
+}
---
+++ DraftMe/Features/Home/HomeViewState.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewState.swift
+//  DraftMe
+//
+//  Created by Kamal Kishor on 14/09/25.
+//
+
+enum HomeViewState: Sendable {
+    case idle
+    case loading
+    case loaded
+    case error
+}
